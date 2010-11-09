363fa7d0f1c36bf9f2a146d9a84ea1c35db04ba6
Jake Levine <levine.jake@gmail.com>
2010-11-08 22:24:33 -0500

    --11.8 v2

--- main.py
+++ main.py
@@ -29,33 +29,67 @@ class Tweets(db.Model):
 
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
+  def get(self):
 
 
-      template_values = {  
-          }
+    template_values = {  
+    }
 
-      path = os.path.join(os.path.dirname(__file__), 'index.html')
-      self.response.out.write(template.render(path, template_values))
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
 class Statuspage(webapp.RequestHandler):
-    def post(self):
-        tweets = Tweets()
-        tweets.username = self.request.get('username')
+  def post(self):
+    tweets = Tweets()
+    tweets.username = self.request.get('username')
 
-        fetched = urlfetch.fetch("http://api.twitter.com/1/statuses/user_timeline.json?screen_name="+tweets.username+"&count=1")
-        statustext = fetched.content
-        loc = statustext.find('"text":"')
-        statusfound = statustext[loc+8:-3]
+    fetched = urlfetch.fetch("http://api.twitter.com/1/statuses/user_timeline.json?screen_name="+tweets.username+"&count=5")
+    statustext = fetched.content
+
+    loc1st = statustext.find('"text":"')
+    status1a = statustext[loc1st+8:]
+    loc1end = status1a.find('"},')
+    status1b = status1a[:loc1end-2]
+
+    loc2st = status1a.find('"text":"')
+    status2a = status1a[loc2st+8:]
+    loc2end = status2a.find('"},')
+    status2b = status2a[:loc2end-2]
+
+    loc3st = status2a.find('"text":"')
+    status3a = status2a[loc3st+8:]
+    loc3end = status3a.find('"},')
+    status3b = status3a[:loc3end-2]
+
+    loc4st = status3a.find('"text":"')
+    status4a = status3a[loc4st+8:]
+    loc4end = status4a.find('"},')
+    status4b = status4a[:loc4end-2]
+
+    loc5st = status4a.find('"text":"')
+    status5a = status4a[loc5st+8:]
+    loc5end = status5a.find('"},')
+    status5b = status5a[:loc5end-2]
 
 
-        content_values = {
-            'status': statusfound,
-            'username':tweets.username,
-            }
 
-        path = os.path.join(os.path.dirname(__file__), 'statuspage.html')
-        self.response.out.write(template.render(path, content_values))
+    statusfound1 = status1b
+    statusfound2 = status2b
+    statusfound3 = status3b
+    statusfound4 = status4b
+    statusfound5 = status5b
+
+    content_values = {
+        'status1': statusfound1,
+        'status2': statusfound2,
+        'status3': statusfound3,
+        'status4': statusfound4,
+        'status5': statusfound5,
+        'username':tweets.username,
+        }
+
+    path = os.path.join(os.path.dirname(__file__), 'statuspage.html')
+    self.response.out.write(template.render(path, content_values))
 
 application = webapp.WSGIApplication(
                                      [('/', MainHandler),
@@ -64,8 +98,8 @@ application = webapp.WSGIApplication(
 	
 def main():
 
-    util.run_wsgi_app(application)
+  util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+  main()
